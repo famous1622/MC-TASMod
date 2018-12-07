e553188c37031791f68f7827400819cf4fca337c
Scribble <ScribbleLP@users.noreply.github.com>
2018-12-07 10:02:10 +0100

    Disable Gui on F3

--- src/main/java/de/tr7zw/tas/InfoGui.java
+++ src/main/java/de/tr7zw/tas/InfoGui.java
@@ -6,13 +6,18 @@ import java.awt.datatransfer.StringSelection;
 
 import org.lwjgl.opengl.GL11;
 
+import cpw.mods.fml.client.config.GuiConfig;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiIngameMenu;
+import net.minecraft.client.gui.GuiLabel;
+import net.minecraft.client.gui.GuiListExtended;
+import net.minecraft.client.gui.GuiPlayerInfo;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiSnooper;
 import net.minecraft.util.ChatComponentText;
 import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.GuiScreenEvent.InitGuiEvent;
@@ -42,7 +47,7 @@ public class InfoGui extends Gui{
 			return;
 		}
 		int height=10;
-		if (enabled){
+		if (enabled&&!(mc.gameSettings.showDebugInfo)){
 			new Gui().drawString(mc.fontRenderer, (Math.round((mc.thePlayer.posX-0.5))+" "+Math.round((mc.thePlayer.posY-1.62))+" "+Math.round((mc.thePlayer.posZ-0.5))), 10, height+1, 0xFFFFFF);
 			new Gui().drawString(mc.fontRenderer, "Pitch: "+Float.toString(mc.thePlayer.rotationPitch), 10, height+11, 0xFFFFFF);
 			new Gui().drawString(mc.fontRenderer, "Yaw: "+Float.toString(mc.thePlayer.rotationYaw), 16, height+21, 0xFFFFFF);
